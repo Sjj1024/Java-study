bff7e72178243480714a9866b0d4db92e92aadaf
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-09 14:02:29 +0800

    Create Demo6.java

---
+++ Demo6.java
@@ -0,0 +1,17 @@
+/*
+类型转换：数据范围从小到大隐式转换，例如long转成float
+
+显式转换，需要手动转换数据类型。例如int转成float
+*/
+
+public class Demo6{
+	public static void main(String[] args){
+		System.out.println(1024);
+		System.out.println(3.14);
+		// 隐式类型转换
+		float num1 = 100;
+		System.out.println(num1);
+		
+		// 
+	}
+}
