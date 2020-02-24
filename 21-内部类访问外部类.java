ec7183089888b50b2f2f9e44bceb88e902054c7e
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-02-24 09:42:55 +0800

    Create 21-内部类访问外部类.java

---
+++ 21-内部类访问外部类.java
@@ -0,0 +1,24 @@
+package DemoNw;
+
+public class Body {
+    private String name = "身体";
+    int num = 40;
+
+    public class Heart{
+        public void tiao(){
+            int num = 30;
+            System.out.println("心脏在跳动");
+            System.out.println(num);
+            System.out.println("内部类的成员属性" + num);
+            System.out.println("外部类的成员属性" + Body.this.num);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
