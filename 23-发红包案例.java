b4993b1b69f02d345c8ef6bbb56185e6c24d2673
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-02-26 11:09:26 +0800

    Update 23-发红包案例.java

--- 23-发红包案例.java
+++ 23-发红包案例.java
@@ -63,4 +63,63 @@ public class Person {
     }
 }
 
+// 群主类：
+package HongBao;
+
+import java.util.ArrayList;
+
+public class QunZhu extends Person {
+    public QunZhu() {
+    }
+
+    public QunZhu(String name, double mymoney) {
+        super(name, mymoney);
+    }
+
+    // 定义发红包的方法，
+    public ArrayList<Double> send(double jine, int geshu){
+        System.out.println("开始发送红包");
+        ArrayList<Double> hblist = new ArrayList<>();
+        if(jine > this.getMymoney()){
+            System.out.println("余额不足，无法发送");
+            return hblist;
+        }
+        double everymoney = jine / geshu;
+        double lossmoney = jine % geshu; // 取余数
+        for (int i = 0; i < geshu; i++) {
+            hblist.add(everymoney);
+        }
+        // 获取到群主余额，然后用余额减去红包后再设置回去
+        this.setMymoney(this.getMymoney() - jine);
+        System.out.println("群主发送了" + jine + "元红包!");
+        return hblist;
+    }
+}
+
+
+// 成员类
+package HongBao;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ChengYuan extends Person {
+    public ChengYuan() {
+    }
+
+    public ChengYuan(String name, double mymoney) {
+        super(name, mymoney);
+    }
+
+    public void receive(ArrayList<Double> hblist){
+//        System.out.println("开始接收红包！");
+        // 随机产生一个红包中的索引，获取其中的红包，然后删除红包数组中的红包
+        int index = new Random().nextInt(hblist.size());
+        double hongbao = hblist.remove(index);
+        // 先获取余额，再将获取到的红包加上余额设置回去
+        double mymoney = this.getMymoney();
+        this.setMymoney(hongbao + mymoney);
+        System.out.println(this.name + "抢到了" + hongbao + "元红包");
+    }
+}
 
