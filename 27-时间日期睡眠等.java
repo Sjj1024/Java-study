a216dd802b0a04683e332d78d26b1228d88ed749
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-03-02 18:10:25 +0800

    Update and rename 27-时间日期.java to 27-时间日期睡眠等.java

---
+++ 27-时间日期睡眠等.java
@@ -0,0 +1,32 @@
+package TimerDemo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Timer {
+    public static void main(String[] args) {
+        Date day = new Date();
+        // 使用day对象输出字符串对象
+        System.out.println(day.toString());
+        // 使用格式化日期输出
+        SimpleDateFormat fmormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat fmormat2 = new SimpleDateFormat("MM-dd");
+        System.out.println("当前时间是:" + fmormat.format(day));
+        System.out.println("当前时间是:" + fmormat2.format(day));
+
+        //使用printf可以快速格式化日期
+        System.out.println("------------------");
+        System.out.printf("%tc\n", day); // %tc表示全部日期信息
+        System.out.printf("%tF\n", day); // 年月日信息
+        System.out.printf("%tT\n", day); // 时分秒信息
+
+        // 使计算机延时睡眠多少秒，必须使用捕获异常来处理抛出的异常
+        try {
+            System.out.printf(new Date() + "\n");
+            Thread.sleep(1000*3);
+            System.out.printf(new Date() + "\n");
+        }catch(Exception e){
+            System.out.printf("获得一个异常");
+        }
+    }
+}
