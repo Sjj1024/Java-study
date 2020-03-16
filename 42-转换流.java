c2ac666282adb913cea89a869ad152808ebb783d
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-03-16 08:54:23 +0800

    Update 42-转换流.java

--- 42-转换流.java
+++ 42-转换流.java
@@ -1,5 +1,7 @@
 package IOliu;
 
+import java.io.*;
+
 /*
 * 主要有四种流对象:
 * FileInputStream  文件输入流：从硬盘上读取二进制数据到内存中，也就是应用中
@@ -11,15 +13,23 @@ package IOliu;
 * BufferedInputStream   ：输入缓冲流，读写数据的时候，可以加快输入输出
 * BufferedOutputStream  ：输出缓冲流，写数据的时候，可以加快写入速度
 *
-* InputStreamReader     ：输入转换流，可以指定编码格式的输入流对象，方便实用
-* OutputStreamReader    ：输出转换流，可以指定编码格式的输出流对象，方便实用
+* BufferedRead  ：字符缓冲输入流，将硬盘中的字符内容读取到内存中
+* BufferedWrite ：字符缓冲输出流，将字符内容输出保存到硬盘中
+*
+* InputStreamReader     ：输入转换流，可以指定编码格式的输入流对象，方便实用,需要传入一个字节输入刘对象，和编码格式
+* OutputStreamReader    ：输出转换流，可以指定编码格式的输出流对象，方便实用，需要传入一个字节输出流对象，和编码格式
 *
 * */
 public class BianMa {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("编码解码-字符编码-就是一个编码规则");
         // utf-8是用的最多的编码，
         // FileRead默认只会读取系统默认编码格式的内容,所以可以使用转换流InputStreanmReader,可以指定指定的码表，OutputStreamReader也是一个转换流
-
+        InputStreamReader input = new InputStreamReader(new FileInputStream("dier.txt"), "utf-8");
+        int read = input.read();
+        while (read != -1) {
+            System.out.println((char)read);
+            read = input.read();
+        }
     }
 }
