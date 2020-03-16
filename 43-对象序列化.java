ea7a3d893f70b81d7cfda0d552502ce8fcdda77a
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-03-16 10:44:52 +0800

    Update 43-对象序列化.java

--- 43-对象序列化.java
+++ 43-对象序列化.java
@@ -1,8 +1,10 @@
 package IOliu;
 
 import ObjThree.Person;
+import demo.list.ArrayListDemo1;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class XuLieHua {
     public static void main(String[] args) throws IOException,ClassNotFoundException {
@@ -28,5 +30,29 @@ public class XuLieHua {
 
         // 类中的静态关键static是和对象没有关系的，而且静态优先于类加载到内存中，所以序列化的时候，是不能保存静态内容的
         // 瞬态关键字也是一样，不能在序列化的时候将内容保存到硬盘中,transient修饰的内容，默认保存的都是默认值
+
+        // 当发生InvalidClassException异常的时候，可能是序列化的时候和反序列化的时候，类中做了修改，导致类的版本号不一样，所以报异常
+        // 解决此问题的方法就是在类中定义自己的版本号，无论类怎么变化，都使用同一个版本号，就不会报错了
+        // 定义自己版本号方法：private static final long serialVersionUID = 42L;后面的版本号随意写
+
+        System.out.println("---------------------------------------------");
+        //序列化集合：将对象保存到集合中，然后序列化的时候，就直接写入集合，读取的时候，读到的也是一个集合，需要强转成集合，然后再遍历内容
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(new Person("王健林", 45));
+        list.add(new Person("王思聪", 23));
+        list.add(new Person("王巴丹", 42));
+        list.add(new Person("王铁蛋", 23));
+        // 创建一个对象序列化流
+        ObjectOutputStream objout = new ObjectOutputStream(new FileOutputStream("listobj.txt"));
+        objout.writeObject(list);
+        objout.close();
+        // 创建一个对象反序列化流
+        ObjectInputStream objin = new ObjectInputStream(new FileInputStream("listobj.txt"));
+        Object three = objin.readObject();
+        ArrayList<Person> list2 = (ArrayList<Person>) three;
+        System.out.println(list2);
+        for (Person person : list2) {
+            System.out.println(person.getName() + person.getAge());
+        }
     }
 }
