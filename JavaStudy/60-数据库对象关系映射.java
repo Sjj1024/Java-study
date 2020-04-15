b23a82c267f367b13d968e54e677eb1cd8a67ff3
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-04-15 11:24:49 +0800

    Update 60-数据库对象关系映射.java

--- JavaStudy/60-数据库对象关系映射.java
+++ JavaStudy/60-数据库对象关系映射.java
@@ -3,6 +3,9 @@ package com.boot.strap.test.Dao;
 import java.util.Date;
 
 public class Studentbean {
+    // 因为数据库和类特别想，数据库表中，每一列都有一个名字和类别属性，那就可以和类中属性关联起来
+    // 比如学生表中，有id，name，age，score，borthday， insert_time，那就可以单独创建一个类，来和它对应起来
+    // 最后查出来每一个数据库中的元素，就可以和这个类绑定，创建一个类对象，从而很简单的使用
     private int id;
     private String name;
     private int age;
