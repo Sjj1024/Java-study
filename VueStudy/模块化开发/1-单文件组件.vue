4c8f9e4d721b63f440aeadc00cd3fc482faa97dd
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-05-20 17:24:37 +0800

    Create 1-单文件组件.vue

---
+++ VueStudy/模块化开发/1-单文件组件.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        单文件组件组成部分
+    </div>
+</template>
+
+<script>
+// 使用export default将vue组件暴露出去，别的文件就可以引入使用了
+export default {
+    data(){
+        return {
+            msg: "单文件组件中的数据"
+        }
+    },
+    methods:{
+        save:function(){
+            console.log("这是单文件组件的方法");
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* 单文件组件的样式表 */
+</style>
