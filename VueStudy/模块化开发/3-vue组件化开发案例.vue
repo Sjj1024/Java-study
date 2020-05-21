41da9d294be418703c71019ff467dbb99691c9ae
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-05-21 20:57:36 +0800

    Create 3-vue组件化开发案例.vue

---
+++ VueStudy/模块化开发/3-vue组件化开发案例.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="data-item-row" :key="i" v-for="(item,i) in items">
+            <div style="width: 200px;margin: 5px;line-height:50px;float: left;">{{item.name}}</div>
+            <div style="width: 200px;margin: 5px;line-height:50px;float: left;">{{item.name}}</div>
+            <div style="width: 200px;margin: 5px;line-height:50px;float: left;">{{item.name}}</div>
+            <div style="width: 200px;margin: 5px;line-height:50px;float: right;">
+                <button class="btn btn-danger btn-xs" style="float: right;margin: 2px;">删除</button>
+                <button class="btn btn-primary btn-xs" data-toggle="modal" data-target="#editRobotDlg" style="float: right;margin: 2px;" @click="onShowRobotEditDialog()">编辑</button>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"agvtable",
+    props:{
+        items:[]
+    },
+    data(){
+        return {
+            title:""
+        }
+    },
+    methods:{
+        
+    },
+    mounted:function(){
+        var robot = new Object();
+        robot.name = "Aubo";
+        this.items.push(robot);
+    }
+}
+</script>
+
+
+<style>
+
+</style>
