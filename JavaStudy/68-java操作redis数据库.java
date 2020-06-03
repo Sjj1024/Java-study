3f9d58abce7ee8275fffa9589d6cfd4456e75b93
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-06-03 09:50:16 +0800

    Update 68-java操作redis数据库.java

--- JavaStudy/68-java操作redis数据库.java
+++ JavaStudy/68-java操作redis数据库.java
@@ -2,6 +2,8 @@ package it.cast.showuser.dao.impl;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 public class JedisTest {
     public static void main(String[] args) {
         // 1.创建jedis连接
@@ -27,9 +29,27 @@ public class JedisTest {
         // System.out.println(hget);
 
         // 删除hash类型的值
-        Long hdel = firstjedis.hdel("myhash", "first");
-        System.out.println(hdel);
+        // Long hdel = firstjedis.hdel("myhash", "first");
+        // System.out.println(hdel);
 
+        // 操作列表类型元素
+        Long lpush = firstjedis.lpush("mylist", "one");
+        System.out.println(lpush);
+
+        Long rpush = firstjedis.rpush("mylist", "ends");
+        System.out.println(rpush);
+
+        // 获取列表元素
+        String mylist = firstjedis.lpop("mylist");
+        System.out.println(mylist);
+
+        // 遍历列表元素
+        List<String> mylist1 = firstjedis.lrange("mylist", 0, -1);
+        for (String s : mylist1) {
+            System.out.println(s);
+        }
+
+        //
 
         // 3.关闭连接
         firstjedis.close();
