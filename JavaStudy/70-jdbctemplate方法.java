bdf8fc4a5e80d6e529039f68ddcdd72068266b52
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-06-16 20:12:58 +0800

    Create 70-jdbctemplate方法.java

---
+++ JavaStudy/70-jdbctemplate方法.java
@@ -0,0 +1,9 @@
+JdbcTemplate主要提供以下五类方法：
+
+
+execute方法：可以用于执行任何SQL语句，一般用于执行DDL语句；
+update方法及batchUpdate方法：update方法用于执行新增、修改、删除等语句；batchUpdate方法用于执行批处理相关语句；
+query方法及queryForXXX方法：用于执行查询相关语句；
+call方法：用于执行存储过程、函数相关语句。
+
+引用连接：https://blog.csdn.net/u011781521/article/details/72598730
