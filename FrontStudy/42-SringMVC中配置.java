e7511e2d0caff351e3bfe1aa8a137fa8b019fb05
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-06-19 17:56:31 +0800

    Update 42-SringMVC中配置.java

--- FrontStudy/42-SringMVC中配置.java
+++ FrontStudy/42-SringMVC中配置.java
@@ -1,5 +1,6 @@
 package it.com.heima.controller;
 
+import it.com.heima.bean.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +16,19 @@ public class FirstController {
     // value 就是路径，RequestMapping中只有一个参数，可以省略value，method表示请求方式
     @RequestMapping(value = "/first", method = {RequestMethod.GET})
     @ResponseBody
-    public String first() {
+    public String first(String name) {
         // 而且spring会自动将请求参数映射到方法的参数中，get请求的话，会将?后面的参数通过String 传递过来，
         // POST请求的话，也可以通过javabean对象映射，其内部使用的就是映射
+        System.out.println("接收到的name是:" + name);
         return "first hello world";
+    }
+
+    // 当封装成JAVABean的例子
+    @RequestMapping("/adduser")
+    @ResponseBody
+    public String beanTest(User user) {
+        // 会自动将传递过来的form表单中name对应的属性和user中的属性匹配，并生成user对象传递进来
+        System.out.println(user);
+        return "nihao javabean";
     }
 }
