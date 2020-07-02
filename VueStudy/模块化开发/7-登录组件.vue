815717fd12ca5e45a68c5c7d70ce4807ffade674
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-07-02 16:09:32 +0800

    Create 7-登录组件.vue

---
+++ VueStudy/模块化开发/7-登录组件.vue
@@ -0,0 +1,85 @@
+<template>
+    <body id="poster">
+        <el-form class="login-container" label-position="left" label-width="0px">
+            <h3 class="login_title">终端硬测应用商店</h3>
+            <el-form-item>
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+        
+            <el-form-item>
+                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+            </el-form-item>
+        
+            <el-form-item style="width: 100%">
+                <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登 录</el-button>
+            </el-form-item>
+        </el-form>
+    </body>
+</template>
+
+<script>
+import axios from "axios";
+
+
+export default {
+    name: "login",
+    data() {
+        return {
+            loginForm: {
+                username: '',
+                password: ''
+            },
+            responseResult: []
+        }
+    },
+    methods: {
+        login() {
+            this.$axios
+                .post('/doLogin', {
+                    username: this.loginForm.username,
+                    password: this.loginForm.password
+                })
+                .then(successResponse => {
+                    if (successResponse.data.code === 200) {
+                        this.$router.replace({path: '/index'})
+                    }
+                })
+                .catch(failResponse => {
+                })
+        }
+    }
+}
+</script>
+
+<style>
+  #poster {
+    background:url("../assets/images/boli.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+    padding: 0;
+  }
+ 
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 220px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+ 
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+    font-size: 24px;
+  }
+</style>
